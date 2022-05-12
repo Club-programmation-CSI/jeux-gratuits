532769dc7d97710414d7a8e5278556458cbe8852
Rafael <rafael.martinezcalvo@orange.fr>
2022-05-12 20:35:32 +0200

    fix epic api
    Co-authored-by: Hyiro <Hyiro@users.noreply.github.com>

--- API/epic.py
+++ API/epic.py
@@ -1,22 +1,31 @@
+from curses import raw
 import requests
 from bs4 import BeautifulSoup
 
 
 def fetchGames():
     response = requests.get(
-        "https://store.epicgames.com/graphql?operationName=getCatalogOffer&variables={%22locale%22:%22fr%22,%22country%22:%22FR%22,%22offerId%22:%22b97151524f9a4815961e9a17ec3c990e%22,%22sandboxId%22:%2225d726130e6c4fe68f88e71933bda955%22}&extensions={%22persistedQuery%22:{%22version%22:1,%22sha256Hash%22:%22ff096572d1065b7058e64c86ce4630bfb5727955056fe910b3f29cb50568fdd7%22}}"
+        "https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=fr&country=FR&allowCountries=FR"
     )
 
     json = response.json()
 
-    rawGame = json["data"]["Catalog"]["catalogOffer"]
-    game = {
-        "name": rawGame["title"],
-        "imageURL": rawGame["keyImages"][0]["url"],
-        "gameLink": "https://store.epicgames.com/fr/free-games",
-        "platform": "epic games",
-    }
-
+    rawGames = json["data"]["Catalog"]["searchStore"]["elements"]
     games = []
-    games.append(game)
+    for rawGame in rawGames:
+        if "discount" not in rawGame["price"]["totalPrice"]:
+            continue
+        if (
+            rawGame["price"]["totalPrice"]["discount"] == 0
+            or rawGame["price"]["totalPrice"]["discountPrice"] != 0
+        ):
+            continue
+        game = {
+            "name": rawGame["title"],
+            "imageURL": rawGame["keyImages"][0]["url"],
+            "gameLink": "https://store.epicgames.com/fr/free-games",
+            "platform": "epic games",
+        }
+        games.append(game)
+
     return games
---
+++ API/test.py
@@ -0,0 +1,3 @@
+import epic
+
+print(epic.fetchGames())
